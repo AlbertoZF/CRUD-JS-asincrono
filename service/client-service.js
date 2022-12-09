4647b729c8cb8bf96aec4eb5374102bd7acb1662
Luis Alberto Zaragoza <alberto@supernova.com.mx>
2022-12-09 16:18:12 -0600

    Reescribiendo con Fetch API

--- service/client-service.js
+++ service/client-service.js
@@ -31,27 +31,29 @@ const crearNuevaLinea = (nombre, email) => {
 
 const table = document.querySelector("[data-table]");
 
+// Fetch API
+const listaClientes = () => fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
-const listaClientes = () => {
-    const promise = new Promise((resolve, reject) => {
-        const http = new XMLHttpRequest();
-        //Abrir http (metodo, url)
-        // CRUD
-        http.open("GET", "http://localhost:3000/perfil");
+// const listaClientes = () => {
+    // const promise = new Promise((resolve, reject) => {
+    //     const http = new XMLHttpRequest();
+    //     //Abrir http (metodo, url)
+    //     // CRUD
+    //     http.open("GET", "http://localhost:3000/perfil");
 
-        http.send();
+    //     http.send();
 
-        http.onload = () => {
-            const response = JSON.parse(http.response);
-            if (http.status >= 400) {
-                reject(response);
-            } else {
-                resolve(response);
-            }
-        };
-    });
-    return promise;
-};
+    //     http.onload = () => {
+    //         const response = JSON.parse(http.response);
+    //         if (http.status >= 400) {
+    //             reject(response);
+    //         } else {
+    //             resolve(response);
+    //         }
+    //     };
+    // });
+    // return promise;
+// };
 
 listaClientes()
     .then((data) => {
