c0fe93933e4d3d64c75b52dae0f003488e17824e
Luis Alberto Zaragoza <alberto@supernova.com.mx>
2022-12-09 16:02:40 -0600

    Lidiando con Promises

--- service/client-service.js
+++ service/client-service.js
@@ -1,8 +1,8 @@
 const crearNuevaLinea = (nombre, email) => {
     const linea = document.createElement("tr");
-    const contenido = 
-    // Backticks `` para porder combinar HTML con JS
-    `
+    const contenido =
+        // Backticks `` para porder combinar HTML con JS
+        `
     <td class="td" data-td>${nombre}</td>
     <td>${email}</td>
     <td>
@@ -31,18 +31,36 @@ const crearNuevaLinea = (nombre, email) => {
 
 const table = document.querySelector("[data-table]");
 
-const http = new XMLHttpRequest();
 
-//Abrir http (metodo, url)
-// CRUD
-http.open("GET", "http://localhost:3000/perfil");
+const listaClientes = () => {
+    const promise = new Promise((resolve, reject) => {
+        const http = new XMLHttpRequest();
+        //Abrir http (metodo, url)
+        // CRUD
+        http.open("GET", "http://localhost:3000/perfil");
 
-http.send();
+        http.send();
 
-http.onload = () => {
-    const data = JSON.parse(http.response);
+        http.onload = () => {
+            const response = JSON.parse(http.response);
+            if (http.status >= 400) {
+                reject(response);
+            } else {
+                resolve(response);
+            }
+        };
+    });
+    return promise;
+};
+
+listaClientes()
+    .then((data) => {
     data.forEach((perfil) => {
         const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
         table.appendChild(nuevaLinea);
     });
-};
+})
+    .catch((error) => alert("Ocurrió un error"));
+
+
+
