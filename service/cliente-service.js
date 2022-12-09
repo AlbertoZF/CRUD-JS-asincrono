507809a542dc73b02fc06a46473e5edeb8ffb12c
Alberto Zaragoza <alberto@supernova.com.mx>
2022-12-09 13:40:39 -0600

    Exibiendo Datos

---
+++ service/cliente-service.js
@@ -0,0 +1,12 @@
+const http = new XMLHttpRequest();
+
+//Abrir http (metodo, url)
+// CRUD
+http.open("GET", "http://localhost:3000/perfil");
+
+http.send();
+
+http.onload = () => {
+    const data = http.response
+    console.log(data);
+}
