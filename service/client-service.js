5e70eb921d97efa7fa4c4b952d6c7f2509ebdee7
Luis Alberto Zaragoza <alberto@supernova.com.mx>
2022-12-09 16:43:08 -0600

    Refactorizando responsabilidades

--- service/client-service.js
+++ service/client-service.js
@@ -1,36 +1,3 @@
-const crearNuevaLinea = (nombre, email) => {
-    const linea = document.createElement("tr");
-    const contenido =
-        // Backticks `` para porder combinar HTML con JS
-        `
-    <td class="td" data-td>${nombre}</td>
-    <td>${email}</td>
-    <td>
-        <ul class="table__button-control">
-        <li>
-            <a
-            href="../screens/editar_cliente.html"
-            class="simple-button simple-button--edit"
-            >Editar</a
-            >
-        </li>
-        <li>
-            <button
-            class="simple-button simple-button--delete"
-            type="button"
-            >
-            Eliminar
-            </button>
-        </li>
-        </ul>
-    </td>
-    `;
-    linea.innerHTML = contenido;
-    return linea;
-}
-
-const table = document.querySelector("[data-table]");
-
 // Fetch API
 const listaClientes = () => fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
@@ -55,14 +22,6 @@ const listaClientes = () => fetch("http://localhost:3000/perfil").then((respuest
     // return promise;
 // };
 
-listaClientes()
-    .then((data) => {
-    data.forEach((perfil) => {
-        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-        table.appendChild(nuevaLinea);
-    });
-})
-    .catch((error) => alert("Ocurrió un error"));
-
-
-
+export const clientServices = {
+    listaClientes,
+};
