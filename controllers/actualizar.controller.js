942567fe288284281a48eb3b374c10a3d539f970
Luis Alberto Zaragoza <alberto@supernova.com.mx>
2022-12-12 20:55:58 -0600

    Exhibiendo datos para Edición

--- controllers/actualizar.controller.js
+++ controllers/actualizar.controller.js
@@ -1,5 +1,6 @@
 import { clientServices } from "../service/client-service.js";
 
+const formulario = document.querySelector("[data-form]");
 
 const obtenerInformacion = () => {
     const url = new URL(window.location);
@@ -19,3 +20,15 @@ const obtenerInformacion = () => {
 }
 
 obtenerInformacion();
+
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+    const url = new URL(window.location);
+    const id = url.searchParams.get("id");
+
+    const nombre = document.querySelector("[data-nombre]").value;
+    const email = document.querySelector("[data-email]").value;
+    clientServices.actualizarCliente(nombre, email, id).then(() => {
+        window.location.href="/screens/edicion_concluida.html"
+    });
+});
